40d99bb67a3729fd4ed8f11ba1b941925b825510
Rishyan Syal <karanrishyan@gmail.com>
2025-02-27 00:14:10 +0100

    Auth Added

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,28 +1,47 @@
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Retrieve the connection string from the configuration
+var dbConnectionString = builder.Configuration.GetValue<string>("SqlConnectionString");
+
 // Add services to the container.
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization(); // Add authorization
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddDapperStores(options => { options.ConnectionString = dbConnectionString; });
+
+// Adding the HTTP Context accessor to be injected. This is needed by the AspNetIdentityUserRepository
+// to resolve the current user.
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddTransient<IAuthenticationService, AspNetIdentityAuthenticationService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-var sqlConnectionString = builder.Configuration.GetValue<string>("SqlConnectionString");
-var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(sqlConnectionString);
-app.MapGet("/", () => $"The API is up . Connection string found: {(sqlConnectionStringFound ? "Yes" : "No")}");
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseAuthentication(); // Add authentication middleware
+app.UseAuthorization(); // Add authorization middleware
 
-app.MapControllers();
+// Map the Identity API endpoints under the 'account' prefix
+app.MapGroup("/account")
+    .MapIdentityApi<IdentityUser>();
+
+var sqlConnectionString = builder.Configuration.GetValue<string>("SqlConnectionString");
+var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(sqlConnectionString);
+app.MapGet("/", () => $"The API is up. Connection string found: {(sqlConnectionStringFound ? "Yes" : "No")}");
+
+// Secure all controllers and endpoints with RequireAuthorization
+app.MapControllers().RequireAuthorization();
 
 app.Run();
