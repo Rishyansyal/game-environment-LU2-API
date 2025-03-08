8414ec4342f03ac524e30c84d48b10f01482f47a
Rishyan Syal <karanrishyan@gmail.com>
2025-03-08 15:43:20 -0300

    Added range id environment2ds

--- WebApi/Models/Environment2d.cs
+++ WebApi/Models/Environment2d.cs
@@ -5,6 +5,7 @@ namespace WebApi.Models;
 public class Environment2d
 {
 
+    [Range(0, 4)]
     public int Id { get; set; }
     public string Name{ get; set; }
     public int MaxLength { get; set; } 
