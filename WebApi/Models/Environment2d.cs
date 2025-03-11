4a188c87a766ee28ddff39fdaf427a425599d7b3
Rishyan Syal <karanrishyan@gmail.com>
2025-03-11 19:23:16 +0100

    Revert "Added range id environment2ds"
    This reverts commit 8414ec4342f03ac524e30c84d48b10f01482f47a.

--- WebApi/Models/Environment2d.cs
+++ WebApi/Models/Environment2d.cs
@@ -5,7 +5,6 @@ namespace WebApi.Models;
 public class Environment2d
 {
 
-    [Range(0, 4)]
     public int Id { get; set; }
     public string Name{ get; set; }
     public int MaxLength { get; set; } 
